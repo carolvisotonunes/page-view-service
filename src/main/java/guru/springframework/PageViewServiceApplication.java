eb289f0b54959f422c2e602128cc3b50a4ec5bc4
Caroline Nunes <carolinenunes@Carolines-MacBook-Pro.local>
2020-11-02 11:38:52 +1100

    integration docker server

---
+++ src/main/java/guru/springframework/PageViewServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class PageViewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageViewServiceApplication.class, args);
+	}
+}
